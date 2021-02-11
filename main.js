26aab2a9f36413620e82359da5e1bf9e71446538
Frederiek107 <f.a.tenbroeke@gmail.com>
2021-02-11 19:38:11 +0100

    added 2nd+3rd assignment

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 async function fetchData() {
-    const data=await axios.get("https://restcountries.eu/rest/v2/name/belgie?fullText=true");
-    console.log(data);
+    const endpoint=await axios.get("https://restcountries.eu/rest/v2/name/belgie?fullText=true");
+    console.log(endpoint);
+    console.log(endpoint.data[0].name + " is situated in " + endpoint.data[0].subregion + ". " +
+        "It has a population of " + endpoint.data[0].population + " people.");
+    console.log("The capital is " + endpoint.data[0].capital) +".";
 }
 
 fetchData();
