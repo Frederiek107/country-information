ecc4b9ed439eca7d683d397b8358fbb4fca71cad
Frederiek107 <f.a.tenbroeke@gmail.com>
2021-02-12 15:36:47 +0100

    added 9th assignment

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 const button = document.getElementById("button");
-button.addEventListener("click", fetchData());
+button.addEventListener("click", showDataButton);
 const input = document.getElementById("input");
-input.addEventListener("keyup", showData);
+input.addEventListener("keyup", showDataKeyboard);
 
-function showData(e) {
+function returnURL() {
+    let inputValue = input.value;
+    let apiURL = "https://restcountries.eu/rest/v2/name/" + inputValue + "?fullText=true";
+    return apiURL;
+}
+
+
+async function showDataButton(e) {
+    const endpoint= await axios.get(returnURL());
+    const country=endpoint.data[0];
+    const page=document.getElementById("mainpage");
+    const result=document.createElement("p");
+    result.setAttribute('style', 'white-space: pre;');
+    result.textContent= country.name + "\n" + await fetchData() + "\n" + await fetchCurrencies() + "\n" + await fetchLanguages();
+    page.appendChild(await fetchFlag());
+    page.appendChild(result);
+}
+
+async function showDataKeyboard(e) {
     const condition=e.keyCode===13;
     if (condition) {
-        showResults();
+        const endpoint= await axios.get(returnURL());
+        const country=endpoint.data[0];
+        const page=document.getElementById("mainpage");
+        const result=document.createElement("p");
+        result.setAttribute('style', 'white-space: pre;');
+        result.textContent= country.name + "\n" + await fetchData() + "\n" + await fetchCurrencies() + "\n" + await fetchLanguages();
+        page.appendChild(await fetchFlag());
+        page.appendChild(result);
     }
 }
 
 async function fetchData() {
-    const endpoint=await axios.get("https://restcountries.eu/rest/v2/name/belgie?fullText=true");
-    console.log(endpoint);
+    const endpoint=await axios.get(returnURL());
     console.log(endpoint.data[0].name + " is situated in " + endpoint.data[0].subregion + ". " +
         "It has a population of " + endpoint.data[0].population + " people.");
     console.log("The capital is " + endpoint.data[0].capital + ".");
@@ -21,10 +45,8 @@ async function fetchData() {
     return result;
 }
 
-fetchData();
-
 async function fetchCurrencies() {
-   const endpoint=await axios.get("https://restcountries.eu/rest/v2/name/belgium?fullText=true");
+   const endpoint=await axios.get(returnURL());
     let string= "The capital is " + endpoint.data[0].capital + " and you can pay with ";
     const currencies= endpoint.data[0].currencies;
    for (const currency of currencies) {
@@ -40,15 +62,13 @@ async function fetchCurrencies() {
    return string;
 }
 
-fetchCurrencies();
-
 async function fetchLanguages() {
-    const endpoint=await axios.get("https://restcountries.eu/rest/v2/name/belgium?fullText=true");
+    const endpoint=await axios.get(returnURL());
     let string="They speak ";
     const languages= endpoint.data[0].languages;
     for (const language of languages) {
         if (language===languages[0]) {
-            string = string + language.name;
+            string = string + language.name + ".";
         }
         if (languages.length===2 && language === languages[1]) {
             string = string + " and " + language.name + ".";
@@ -64,10 +84,8 @@ async function fetchLanguages() {
     return string;
 }
 
-fetchLanguages();
-
 async function fetchFlag() {
-    const endpoint=await axios.get("https://restcountries.eu/rest/v2/name/belgium?fullText=true");
+    const endpoint=await axios.get(returnURL());
     const flagItem = document.createElement("img");
     const country=endpoint.data[0];
     flagItem.setAttribute("src", country.flag);
@@ -76,7 +94,7 @@ async function fetchFlag() {
 }
 
 async function showResults() {
-    const endpoint=await axios.get("https://restcountries.eu/rest/v2/name/belgium?fullText=true");
+    const endpoint=await axios.get(returnURL());
     const country=endpoint.data[0];
     const page=document.getElementById("mainpage");
     const result=document.createElement("p");
@@ -84,7 +102,6 @@ async function showResults() {
     result.textContent= country.name + "\n" + await fetchData() + "\n" + await fetchCurrencies() + "\n" + await fetchLanguages();
     page.appendChild(await fetchFlag());
     page.appendChild(result);
-    console.log(page);
 }
 
 
